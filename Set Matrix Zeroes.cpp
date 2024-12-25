d934315b08af46e2f46e786603fd0ba0125d5803
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2024-12-25 16:29:07 +0530

    Matrix
    leetcode
    O(1) Space complexity

---
+++ Set Matrix Zeroes.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+// O(1) Space Complexity 
+    void setZeroes(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) return;
+        int x=1,y=1;
+        int n=matrix.size(), m=matrix[0].size();
+        
+        for(int j=0;j<m;j++){
+            if(matrix[0][j]==0){
+                x=0;
+                // break;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]==0){
+                y=0;
+                // break;
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+
+        //first row iteration
+        for(int i=1; i<n;i++){
+            if(matrix[i][0]==0){
+                for(int j=0;j<m;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        for(int j=1; j<m;j++){
+            if(matrix[0][j]==0){
+                for(int i=0;i<n;i++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(x==0){
+            for(int j=0;j<m;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(y==0){
+            for(int j=0;j<n;j++){
+                matrix[j][0]=0;
+            }
+        }
+
+    }
+};
