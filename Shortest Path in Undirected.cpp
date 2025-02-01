5959551b37387bcfe92a71ed88bf4a82643f0063
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-02-01 16:35:19 +0530

    graph

---
+++ Shortest Path in Undirected.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    // Function to find the shortest path from source to all other nodes
+    vector<int> shortestPath(vector<vector<int>>& adj, int src) {
+        int n=adj.size();
+        vector<int> dist(n,1e9);
+        dist[src]=0;
+        queue<int> q;
+        q.push(src);
+        
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            
+            for(auto it:adj[node]){
+                if(dist[node]+1 < dist[it]){
+                    dist[it] = dist[node]+1;
+                    q.push(it);
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(dist[i]==1e9){
+                dist[i]=-1;
+            }
+        }
+        
+        return dist;
+    }
+};
