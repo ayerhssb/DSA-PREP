a85fa3e29b4cd8aafdea7e16a79184a523e8e014
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-03-11 22:59:57 +0530

    graph

---
+++ Bellman-Ford.cpp
@@ -0,0 +1,44 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    /*  Function to implement Bellman Ford
+     *   edges: vector of vectors which represents the graph
+     *   src: source vertex
+     *   V: number of vertices
+     */
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // Code here
+        int e=edges.size();
+        vector<int> dist(V, 1e8);
+        dist[src] =0;
+        for(int i=0;i<V;i++){
+            bool relaxed =0;
+            
+            for(auto &it: edges){
+                int u=it[0];
+                int v=it[1];
+                int w=it[2];
+                
+                if(dist[u]!=1e8 && dist[v]>dist[u]+w){
+                    dist[v]=dist[u]+w;
+                    relaxed=1;
+                }
+            }
+            
+            if(!relaxed) break;
+        }
+        
+        for(auto &it: edges){
+                int u=it[0];
+                int v=it[1];
+                int w=it[2];
+                
+                if(dist[u]!=1e8 && dist[v]>dist[u]+w){
+                    return {-1};
+                }
+        }
+        
+        return dist;
+    }
+};
