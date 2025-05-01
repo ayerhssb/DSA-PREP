aba51fc8685f828ce993b0edd477e83a392e01e1
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-05-02 03:48:10 +0530

    binary search
    greedy
    #lc_hard

---
+++ Maximum Number of Tasks You Can Assign.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    bool canAssign(int mid, vector<int>& tasks, vector<int>& workers, int pills, int strength){
+        multiset<int> usable_workers(workers.end()-mid, workers.end());
+        for(int i=mid-1;i>=0;i--){
+            auto curr_worker = --usable_workers.end();
+
+
+            if(*curr_worker < tasks[i]){
+                if(pills<=0) return false;
+                auto weakest_worker = usable_workers.lower_bound(tasks[i] - strength);
+
+                if(weakest_worker==usable_workers.end()){
+                    return false;
+                }
+
+                pills--;
+                usable_workers.erase(weakest_worker);
+            }
+            else{
+                usable_workers.erase(curr_worker);
+            }
+        }
+        return true;
+    }
+    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
+        sort(workers.begin(), workers.end());
+        sort(tasks.begin(),tasks.end());
+
+        int low=0, high= min(workers.size(),tasks.size());
+        int assigned=0;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            
+            if(canAssign(mid,tasks,workers,pills,strength)){
+                assigned = mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return assigned;
+    }
+};
