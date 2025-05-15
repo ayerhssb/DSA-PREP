2032f32c54719662a85a95aa4d5d68813e276453
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-05-15 12:53:10 +0530

    recursion

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int helper(int i, vector<int>& dp){
+        if(dp[i]!=-1){return dp[i];}
+        if(i==2|| i==1){return i;}
+        return dp[i] = helper(i-1, dp)+helper(i-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        int ans = helper(n,dp);
+        return ans;
+    }
+};
