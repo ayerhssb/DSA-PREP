4ac19ba4124d3bd9dfd5a5ee31d7cd7c64edacb8
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-06-01 09:53:15 +0530

    array

--- Set Matrix Zeroes.cpp
+++ Set Matrix Zeroes.cpp
@@ -57,3 +57,34 @@ public:
 
     }
 };
+
+
+
+
+//Another brute approach
+void setZeroes(vector<vector<int>>& matrix) {
+        // if(matrix.empty()||matrix[0].empty()) return;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<int> x, y;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    x.push_back(i);
+                    y.push_back(j);
+                }
+            }
+        }
+
+        for(int i=0;i<x.size();i++){
+            for(int j=0;j<n;j++){
+                matrix[x[i]][j] = 0;
+            }
+        }
+        for(int i=0;i<y.size();i++){
+            for(int j=0;j<m;j++){
+                matrix[j][y[i]] = 0;
+            }
+        }
+
+}
