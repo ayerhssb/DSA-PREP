5dba90985f87b8c7e2c1322ce7377f065e44d3c3
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-06-01 11:09:53 +0530

    array

---
+++ Next Permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i1 = -1;
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i - 1] < nums[i]) {
+                i1 = i - 1;
+                break;
+            }
+        }
+        if (i1 == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] > nums[i1]) {
+                swap(nums[i], nums[i1]);
+                reverse(nums.begin() + i1 + 1, nums.end());
+                return;
+            }
+        }
+    }
+};
