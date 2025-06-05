2c2ca573928c8f361f71150fa0cab75e8937f377
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-06-05 14:59:36 +0530

    array

--- Longest Subarray with Sum K.cpp
+++ Longest Subarray with Sum K.cpp
@@ -1,3 +1,29 @@
+//set approach --> O(N)
+
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        unordered_map<int,int> mpp;
+        int prefixSum=0;
+        int maxi=0;
+        for(int i=0;i<arr.size();i++){
+            prefixSum+=arr[i];
+            if(prefixSum == k){
+                maxi=max(maxi, i+1);
+            }
+            else if(mpp.find(prefixSum-k)!=mpp.end()){
+                maxi = max(maxi, i-mpp[prefixSum-k]);
+            }
+            if(mpp.find(prefixSum)==mpp.end()){
+                mpp[prefixSum]=i;
+            }
+        }
+        return maxi;
+    }
+};
+
+
+//map
 class Solution {
   public:
     int longestSubarray(vector<int>& arr, int k) {
