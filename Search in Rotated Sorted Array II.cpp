410a370fde292a9944219aecb1ba49eb4a74a454
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-07-27 12:44:58 +0530

    binary search

--- Search in Rotated Sorted Array II.cpp
+++ Search in Rotated Sorted Array II.cpp
@@ -19,3 +19,44 @@ public:
        return false;
     }
 };
+
+//revision 1:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+public:
+    bool searchRotatedSorted(int arr[], int n, int k){
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid = (low+high)>>1;
+            if(arr[mid]==k){return 1;}
+            else if(arr[low]>=arr[high] && arr[mid]<=arr[low]){
+                if(k>arr[mid] && k<arr[high]){
+                    low=mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+            else if(arr[low]>=arr[high] && arr[mid]>=arr[low]){
+                if(k<arr[mid] && k>arr[low]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(k>arr[mid]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+        return 0;
+    }
+};
