3e953a502d35b5612b08feca769f69e35d590b50
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-07-29 11:30:07 +0530

    linked list

--- Length of cycle.cpp
+++ Length of cycle.cpp
@@ -1,3 +1,52 @@
+// constant space complexity: tc-> O(N)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+class Node {
+    public:
+        int data;
+        Node* next;
+    public:
+        Node(int value, Node* address){
+            data = value;
+            next = address;
+        }
+    public:
+        Node(int value){
+            data = value;
+            next = NULL;
+        }
+};
+*/
+
+class solution {
+public:
+    int lengthOfCycle(Node* head){
+        Node* slow = head;
+        Node* fast = head;
+        while(1){
+            if(!fast->next || !slow->next){return 0;}
+            slow=slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                int cnt=1;
+                fast=fast->next;
+                while(fast!=slow){
+                    fast=fast->next;
+                    cnt++;
+                }
+                return cnt;
+            }
+        }
+        return 0;
+    }
+};
+
+
+// O(N) space complexity.... tc-> O(N)
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -38,3 +87,6 @@ public:
     }
 
 };
+
+
+
