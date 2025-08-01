062ca38b71bc3d4a504071c7a77c1a27ea280432
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-08-02 00:17:05 +0530

    array
    lc potd

--- Pascal's Triangle.cpp
+++ Pascal's Triangle.cpp
@@ -22,3 +22,30 @@ public:
         return ans;
     }
 };
+
+
+//Revision:
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        for(int i=0;i<numRows;i++){
+            vector<int> list(i+1,0);
+            if(i==0){list[0]=1;ans.push_back(list);continue;}
+            if(i==1){
+                list[0]=1;
+                list[1]=1;ans.push_back(list);temp=list;continue;
+            }
+            list[0]=1;
+            list[i]=1;
+            for(int j=1;j<i;j++){
+                list[j] = temp[j-1] + temp[j];
+            }
+            temp=list;
+            ans.push_back(list);
+        }
+        return ans;
+    }
+};
