927416af196ff30fda71c301c76cc44a20684765
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-08-07 12:08:49 +0530

    dp on stocks

--- Best Time to Buy and Sell Stock.cpp
+++ Best Time to Buy and Sell Stock.cpp
@@ -9,3 +9,17 @@ public:
         return maxi;
     }
 };
+
+//revision:
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy=prices[0], mini = prices[0],profit=0;
+        for(int i=1;i<prices.size();i++){
+            profit = max(profit, prices[i]-buy);
+            mini = min(mini,prices[i]);
+            buy=mini;
+        }
+        return profit;
+    }
+};
