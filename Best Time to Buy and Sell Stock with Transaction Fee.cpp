32fc75c1aa07bf74e8320b61ad797081eaabd4d1
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-08-07 22:41:20 +0530

    dp on stocks

--- Best Time to Buy and Sell Stock with Transaction Fee.cpp
+++ Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -1,3 +1,4 @@
+//Tabulation
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
@@ -15,5 +16,30 @@ public:
             }
         }
         return dp[0][1];
+    }
+};
+
+
+//Space optimized
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n=prices.size();
+        // vector<vector<int>> dp(n+1, vector<int>(2,0));
+        vector<int> after(2,0);
+        vector<int> curr(2,0);
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy =0;buy<=1;buy++){
+                if(buy){
+                    curr[buy] = max(-prices[ind]+ after[0], 0+after[1]);
+                }
+                else{
+                    curr[buy] = max(prices[ind]-fee+after[1], 0+after[0]);
+                }
+            }
+            after=curr;
+        }
+        return curr[1];
     }
 };
