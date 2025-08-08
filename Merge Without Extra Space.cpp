6a41662265c8be32316ecc9d063c9f0bb9731a76
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-08-08 16:49:29 +0530

    array

--- Merge Without Extra Space.cpp
+++ Merge Without Extra Space.cpp
@@ -17,3 +17,24 @@ class Solution {
         sort(b.begin(),b.end());
     }
 };
+
+
+//revision:
+
+#include <bits/stdc++.h>
+using namespace std;
+class solution {
+public:
+    void merge(long arr1[], long arr2[], int n, int m) {
+        int p1=n-1,p2=0;
+        while(p1>=0 || p2<m){
+            if(arr1[p1]>arr2[p2]){
+                swap(arr1[p1],arr2[p2]);
+                p2++;p1--;
+            }
+            else{break;}
+        }
+        sort(arr1,arr1+n);
+        sort(arr2, arr2+m);
+    }
+};
