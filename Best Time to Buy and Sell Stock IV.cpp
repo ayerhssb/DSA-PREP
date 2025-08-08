1de44609b871002ce07392ec5af26fc40afd3597
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-08-09 01:17:41 +0530

    revision- dp on stocks

--- Best Time to Buy and Sell Stock IV.cpp
+++ Best Time to Buy and Sell Stock IV.cpp
@@ -20,3 +20,30 @@ public:
 
     }
 };
+
+//revision:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:    
+    int maxProfit(vector<int>& arr, int k) {
+        int n=arr.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1,0)));
+        for(int i=n-1;i>=0;i--){
+            for(int buy = 0;buy<=1;buy++){
+                for(int cap=1;cap<=k;cap++){
+                    if(buy){
+                        dp[i][buy][cap] = max(-arr[i]+dp[i+1][0][cap], 0+dp[i+1][1][cap]);
+                    }
+                    else{
+                        dp[i][buy][cap] = max(arr[i]+dp[i+1][1][cap], 0+dp[i+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][k];
+        
+    }    
+};
