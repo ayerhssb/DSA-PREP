5457a8a8f7d928b0bf9036a53206a6b1be4b96a2
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-08-16 16:05:03 +0530

    linked list

--- Merge K sorted linked lists.cpp
+++ Merge K sorted linked lists.cpp
@@ -1,3 +1,49 @@
+//revision:
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Comp{
+public:
+    bool operator()(ListNode* a, ListNode* b){
+        return a->val > b->val;
+    }
+
+};
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode* , vector<ListNode*>, Comp> pq;
+        for(ListNode* head: lists){
+            if(head!=nullptr)pq.push(head);
+        }
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr= dummy;
+        while(!pq.empty()){
+            ListNode* top = pq.top();
+            pq.pop();
+            curr->next=top;
+            // curr=top;
+            if(top->next!=nullptr){
+                pq.push(top->next);
+            }
+            top=top->next;
+            curr=curr->next;
+        }
+        return dummy->next;
+    }
+};
+
+
 /*Linked list Node structure
 
 struct Node
