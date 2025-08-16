920208fd414c766de428cf06e4051a1285ca3d7b
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-08-16 22:10:46 +0530

    greedy

--- Non-overlapping Intervals.cpp
+++ Non-overlapping Intervals.cpp
@@ -1,3 +1,25 @@
+//revision (leetcode):
+
+class Solution {
+public:
+    static bool comp(const vector<int>&a, const vector<int>& b){
+        return a[1] < b[1];
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),comp);
+        int prev=0,n=intervals.size(), ans=0;
+        for(int i=1;i<n;i++){
+            if(intervals[prev][1]>intervals[i][0]){
+                ans++;
+            }
+            else{
+                prev=i;
+            }
+        }
+        return ans;
+    }
+};
+
 class Solution {
   public:
     static bool sortcol(const vector<int> &v1,const vector<int> &v2) { 
