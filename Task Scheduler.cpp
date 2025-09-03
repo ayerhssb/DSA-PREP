2ca3330c0f2cf37ef31a8df6ea35ff2b0fd50f45
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-09-03 18:10:32 +0530

    Hashing

--- Task Scheduler.cpp
+++ Task Scheduler.cpp
@@ -1,3 +1,20 @@
+//Revision:
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        //using hashmap
+        // result will be max(((n+1)*(f-1)+X), size)
+        int f=0,X=0, size=tasks.size();
+        unordered_map<char, int> mpp;
+        for(auto it:tasks){mpp[it]++; if(f<mpp[it]){f=mpp[it];}}
+        for(auto it:mpp){if(it.second==f)X++;}
+        int ans = (n+1)*(f-1) + X;
+        return max(ans,size);
+    }
+};
+
+//first time
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
