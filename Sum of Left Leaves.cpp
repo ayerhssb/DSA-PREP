d59e41b2dfd1e38e46ae57cb1f1a4b5dca065308
ayerhssb <123381332+ayerhssb@users.noreply.github.com>
2025-09-03 23:07:23 +0530

    binary tree

--- Sum of Left Leaves.cpp
+++ Sum of Left Leaves.cpp
@@ -1,3 +1,31 @@
+//2:
+
+class Solution {
+public:
+    bool isleaf(TreeNode* root){
+        if(!root){return false;}
+        if(!root->right && !root->left){return true;}
+        return false;
+    }
+    // int ans=0;
+    void helper(TreeNode* root, int& ans){
+        if(!root){return;}
+        if(isleaf(root->left)){ans+=root->left->val;}
+        // if(root->left){
+        //     if(isleaf(root->left)){
+        //         ans+=root->left->val;
+        //     }
+        // }
+        helper(root->left,ans);
+        helper(root->right,ans);
+    }
+    int sumOfLeftLeaves(TreeNode* root) {
+        int ans=0;
+        helper(root,ans);
+        return ans;
+    }
+};
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
