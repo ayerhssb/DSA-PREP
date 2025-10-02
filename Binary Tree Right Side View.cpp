de5ccffb83c35f681267b0dd7ad4f76a8883175b
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2025-10-02 11:21:29 +0530

    binary tree

--- Binary Tree Right Side View.cpp
+++ Binary Tree Right Side View.cpp
@@ -1,3 +1,43 @@
+//revision:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/*class Node {
+  public:
+  int data;
+  Node* left;
+  Node* right;
+
+  Node(int data) : data(data), left(nullptr), right(nullptr) {}
+};*/
+
+class Solution {
+public:
+    vector<int> rightView(Node* root) {
+        vector<int> ans;
+        if(!root){return ans;}
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            ans.push_back(q.front()->data);
+            while(n--){
+                Node* temp=q.front();
+                q.pop();
+                
+                if(temp->right){
+                    q.push(temp->right);
+                }
+                if(temp->left){
+                    q.push(temp->left);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
