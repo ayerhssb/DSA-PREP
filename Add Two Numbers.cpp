cb797fa180fb1e05022607638d6be3ca0f9e9d91
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2025-10-16 04:50:33 +0530

    linked list

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -1,3 +1,60 @@
+//rev - 40mins :/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* head1, ListNode* head2) {
+        // ListNode* head1=l1;
+        // ListNode* head2=l2;
+        ListNode* dummy = new ListNode(-1) ;
+        ListNode* temp=dummy;
+        int carry=0;
+        while(head1 && head2){
+            int sum = head1->val + head2->val+carry;
+            if(sum>9){carry=1;sum=sum%10;}else{carry=0;}
+            ListNode* add= new ListNode(sum);
+            temp->next = add;
+            temp=temp->next;
+            head1=head1->next;
+            head2=head2->next;
+        }
+        while(head1){
+            int sum = head1->val+carry;
+            if(sum>9){carry=1;sum=sum%10;}else{carry=0;}
+            ListNode* add= new ListNode(sum);
+            temp->next = add;
+            temp=temp->next;
+            head1= head1->next;
+        }
+        while(head2){
+            int sum = head2->val+carry;
+            if(sum>9){carry=1;sum=sum%10;}else{carry=0;}
+            ListNode* add= new ListNode(sum);
+            temp->next = add;
+            temp=temp->next;
+            head2= head2->next;
+        }
+        if(carry){
+            ListNode* add= new ListNode(carry);
+            temp->next = add;
+            temp=temp->next;
+        }
+        temp->next=NULL;
+        return dummy->next;
+    }
+};
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
