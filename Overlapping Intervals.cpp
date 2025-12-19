7fd6608f353326fca6899fac27c96595cba83aa5
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2025-12-19 19:43:31 +0530

    gfg potd

--- Overlapping Intervals.cpp
+++ Overlapping Intervals.cpp
@@ -1,3 +1,28 @@
+//2nd time:
+
+class Solution {
+  public:
+    vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+        vector<vector<int>> ans;
+        sort(arr.begin(),arr.end(),[](const vector<int> &a, const vector<int> &b){
+            return a[0]<b[0];
+        });
+        int start=arr[0][0], end=arr[0][1];
+        for(int i=0;i<arr.size()-1;i++){
+            if(end>=arr[i+1][0]){
+                end=max(end,arr[i+1][1]);
+            }
+            else{
+                ans.push_back({start,end});
+                start=arr[i+1][0];
+                end=arr[i+1][1];
+            }
+        }
+        ans.push_back({start,end});
+        return ans;
+    }
+};
+
 class Solution {
   public:
     vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
