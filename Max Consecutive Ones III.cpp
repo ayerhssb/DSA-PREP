448d52984ecc370c9249f9e0dba8515a5d792eed
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2025-12-24 23:28:51 +0530

    sliding window

--- Max Consecutive Ones III.cpp
+++ Max Consecutive Ones III.cpp
@@ -1,3 +1,27 @@
+//revision:
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l=0,r=0,cnt=0,maxi=0;
+        while(r<nums.size()){
+            if(k==0&&nums[r]==0){
+                while(nums[l]!=0){
+                    l++;
+                }
+                l++;
+                r++;
+            }
+            else if(nums[r]==0){k--;r++;}
+            else{
+                r++;
+            }
+            cnt=r-l;
+            maxi=max(maxi,cnt);
+        }
+        return maxi;
+    }
+};
+
 #include<bits/stdc++.h>
 using namespace std;
 
