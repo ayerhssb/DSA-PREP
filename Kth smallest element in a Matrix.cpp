307f71151983f8700f65dabe8e44e4bdc3384307
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2025-12-27 18:03:00 +0530

    gfg potd

--- Kth smallest element in a Matrix.cpp
+++ Kth smallest element in a Matrix.cpp
@@ -1,3 +1,27 @@
+//better approach
+//TC- O(n^2* log(k)) and SC- O(k)
+
+class Solution {
+  public:
+    int kthSmallest(vector<vector<int>> &mat, int k) {
+        int n=mat.size(), m=mat[0].size();
+        priority_queue<int> pq;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int cur=mat[i][j];
+                pq.push(cur);
+                if(pq.size()>k){
+                    pq.pop();
+                }
+            }
+        }
+        return pq.top();
+    }
+};
+
+
+//TC- O(n^2* log(n^2)) and SC- O(n*m)
 class Solution {
   public:
     int kthSmallest(vector<vector<int>> &mat, int k) {
