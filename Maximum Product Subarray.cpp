cd713eed5aed00a7fa8441d813cfb0396405d860
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-02-08 23:40:04 +0530

    gfg potd

--- Maximum Product Subarray.cpp
+++ Maximum Product Subarray.cpp
@@ -1,3 +1,25 @@
+//rev:
+
+class Solution {
+  public:
+    int maxProduct(vector<int> &arr) {
+        int lmax=INT_MIN,rmax=INT_MIN,lprod=1,rprod=1,n=arr.size();
+        if(n==1){return arr[0];}
+        for(int i=0;i<n;i++){
+            if(lprod==0){lprod=1;}
+            lprod*=arr[i];
+            lmax=max(lmax,lprod);
+        }
+        for(int i=n-1;i>=0;i--){
+            if(rprod==0){rprod=1;}
+            rprod*=arr[i];
+            rmax=max(rmax,rprod);
+        }
+        return max(lmax,rmax);
+    }
+};
+
+
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
