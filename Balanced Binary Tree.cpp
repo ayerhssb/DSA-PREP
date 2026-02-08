039fda2c6387d12b7819e5044c7c758b2654523a
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-02-09 01:54:27 +0530

    lc potd

--- Balanced Binary Tree.cpp
+++ Balanced Binary Tree.cpp
@@ -1,3 +1,36 @@
+//rev:
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int helper(TreeNode* root){
+        if(!root){return 0;}
+        int l= helper(root->left);
+        int r=helper(root->right);
+        if(l==-1 || r==-1){return -1;}
+        if(abs(l-r)>1){return -1;}
+        return 1+max(l,r);
+    }
+    bool isBalanced(TreeNode* root) {
+        // if(!root){return true;}
+        if(helper(root)==-1){return false;}
+        return true;
+    }
+};
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
