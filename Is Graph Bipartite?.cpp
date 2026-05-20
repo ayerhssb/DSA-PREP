b3971c01f5e5e82d7fc5afe7a775d508290ec056
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-05-20 23:12:02 +0530

    graph

--- Is Graph Bipartite?.cpp
+++ Is Graph Bipartite?.cpp
@@ -1,3 +1,35 @@
+//revision:
+
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& graph, int node, int col, vector<int>& color){
+        color[node]=col;
+
+        for(auto it: graph[node]){
+            if(color[it]==-1){
+                if(!dfs(graph, it, !col, color)){return false;}
+            }
+            else{
+                if(color[it]==col){return false;}
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<int> color(graph.size(),-1);
+        for(int i=0;i<graph.size();i++){
+            if(color[i]==-1){if(!dfs(graph, i, 0, color)){return false;}}
+        }
+        return true;
+    }
+};
+
+
+
+
+
+
+
 class Solution {
 public:
     bool dfs(int node,int col, vector<vector<int>>& graph, vector<int> &color){
