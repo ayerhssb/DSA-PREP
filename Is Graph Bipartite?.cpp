459c239dbd23c4148eda0310f7fb1d432e892006
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-05-21 10:02:07 +0530

    graph

--- Is Graph Bipartite?.cpp
+++ Is Graph Bipartite?.cpp
@@ -1,3 +1,59 @@
+//BFS:
+
+
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& graph, int node, int col, vector<int>& color){
+        color[node]=col;
+
+        for(auto it: graph[node]){
+            if(color[it]==-1){
+                if(!dfs(graph, it, !col, color)){return false;}
+            }
+            else{
+                if(color[it]==col){return false;}
+            }
+        }
+        return true;
+    }
+
+    bool bfs(vector<vector<int>>& graph, int node, vector<int>& color){
+        queue<int> q;
+        q.push(node);
+        // color[node]=col;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it: graph[node]){
+                if(color[it]==-1){
+                    color[it]= !color[node];
+                    q.push(it);
+                }
+                else{
+                    if(color[it]==color[node]){return false;}
+                }
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<int> color(graph.size(),-1);
+        //DFS:
+        // for(int i=0;i<graph.size();i++){
+        //     if(color[i]==-1){if(!dfs(graph, i, 0, color)){return false;}}
+        // }
+        // return true;
+
+        //BFS:
+        for(int i=0;i<graph.size();i++){
+            if(color[i]==-1){color[i]=1; if(!bfs(graph, i, color)){return false;}}
+        }
+        return true;
+        
+    }
+};
+
+
 //revision:
 
 class Solution {
