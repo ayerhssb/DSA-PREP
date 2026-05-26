31b5dc1ce7de527f00d13c2c3ef03e9d521571f4
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-05-26 12:42:01 +0530

    Trie

--- Implement Trie (Prefix Tree).cpp
+++ Implement Trie (Prefix Tree).cpp
@@ -1,3 +1,91 @@
+//revision::
+
+struct Node{
+    Node* links[26] = {nullptr};
+    bool flag;
+    bool iscontain(char ch){
+        return links[ch-'a']!=nullptr;
+    }
+    void put(char ch, Node* node){
+        links[ch-'a']=node;
+    }
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+    bool getend(){
+        return flag;
+    }
+    void setend(){
+        flag=true;
+    }
+
+    ~Node(){
+        for(int i=0;i<26;i++){
+            if(links[i]!=nullptr){
+                delete links[i];
+                links[i]=nullptr;
+            }
+        }
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    ~Trie(){
+        delete root;
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+        for(char ch:word){
+            if(!node->iscontain(ch)){
+                node->put(ch,new Node());
+            }
+            node= node->get(ch);
+        }
+        node->setend();
+    }
+    
+    bool search(string word) {
+        Node* node=root;
+        for(char ch:word){
+            if(!node->iscontain(ch)){
+                return false;
+            }
+            node=node->get(ch);
+        }
+        if(!node->getend()){return false;}
+        return true;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node=root;
+        for(auto it:prefix){
+            if(!node->iscontain(it)){
+                return false;
+            }
+            node=node->get(it);
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
+
+
+
+
 struct Node{
     Node* links[26] = {nullptr};
 
