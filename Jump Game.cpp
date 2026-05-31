b6dd33afe6497ad73bd4c8d7959d8b366a0d7252
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-05-31 14:18:38 +0530

    dp

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -1,3 +1,29 @@
+//dp:
+
+class Solution {
+public:
+    bool helper(vector<int>& nums, int idx, vector<int>& dp){
+        if(idx>=nums.size()-1){
+            return true;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+
+        for(int cnt=1;cnt<=nums[idx];cnt++){
+            if(helper(nums,idx+cnt,dp)){dp[idx] = 1;return true;}
+        }
+        dp[idx]=0;
+        return false;
+    }
+    bool canJump(vector<int>& nums) {
+        int n =nums.size();
+        vector<int> dp(n,-1);
+        return helper(nums,0,dp);
+    }
+};
+
+
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
