eeed2c171883048cff039bf0aabb58de3e70c421
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-06-18 15:59:36 +0530

    nxtw potd

---
+++ Bubble Sort in Decreasing Order.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+    public:
+    void bubbleSort(int arr[], int n){
+        for(int i=n-1;i>=0;i--){
+            int didswap=0;
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]<arr[j+1]){
+                    swap(arr[j], arr[j+1]);
+                    didswap=1;
+                }
+            }
+            if(didswap==0){
+                break;
+            }
+        }
+    }
+};
