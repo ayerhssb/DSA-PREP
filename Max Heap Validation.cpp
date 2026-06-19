335bd364220ab5c934093f3b06d56d96daad63c5
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-06-19 19:08:15 +0530

    nxtw potd

---
+++ Max Heap Validation.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class solution {
+    public:
+        bool maxHeapValidation(vector<int>& arr) {
+            int n=arr.size();
+                for(int i=0;i<n;i++){
+                    if((2*i+1)<n && arr[i]<arr[2*i+1]){
+                       
+                        return false;
+                    }
+                    if((2*i+2)<n && arr[i]<arr[2*i+2]){
+                        return false;
+                    }
+                }
+        return true;
+        }
+
+};
