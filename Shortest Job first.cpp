cf2b9873dea0e04109e460bd9aa54fcf74f70cad
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-06-20 13:42:08 +0530

    nxtw potd

--- Shortest Job first.cpp
+++ Shortest Job first.cpp
@@ -1,3 +1,24 @@
+//rev:
+
+typedef long long ll;
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution{
+    public:
+    int shortestJobFirst(vector<int> &d) {
+        int n=d.size();
+        ll ans=0,total=0;
+        sort(d.begin(),d.end());
+        for(int i=0;i<n-1;i++){
+            ans+=d[i];
+            total = total+ans;
+        }
+        return (total/n);
+    }
+};
+// -------------------------------------------------
+
 class Solution {
   public:
     long long solve(vector<int>& bt) {
