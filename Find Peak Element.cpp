37b3565576ff4997b80862a0ef8424e75af03c8c
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-06-22 14:45:15 +0530

    nxtw potd

--- Find Peak Element.cpp
+++ Find Peak Element.cpp
@@ -1,3 +1,26 @@
+// rev::
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+public:
+    int findPeakElement(int arr[], int n) {
+        if(n==1){return arr[0];}
+        int ans;
+        if(arr[0]>arr[1]){ans=0;}
+        if(arr[n-1]>arr[n-2]){ans=n-1;}
+        for(int i=1;i<n-1;i++){
+            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+                ans=i;break;
+            }
+        }
+        return ans;
+    }
+};
+
+// ---------------------------------------------
+
 #include <bits/stdc++.h>
 using namespace std;
 
