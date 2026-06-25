6964a35d2f3a9adc11a3c22b3dd1f9588d545a6c
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-06-25 14:43:24 +0530

    nxtw potd

--- Merge k Sorted Arrays.cpp
+++ Merge k Sorted Arrays.cpp
@@ -1,3 +1,36 @@
+//rev:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+    vector<int> mergeKSortedArrays(vector<vector<int>>& arr, int k) {
+        vector<int> ans;
+        priority_queue< pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>> , greater<pair<int,pair<int,int>>>> pq;
+        for(int i=0;i<k;i++){
+            if(!arr[i].empty()){
+                pq.push({arr[i][0], {i,0}});
+            }
+        }
+        while(!pq.empty()){
+            int small = pq.top().first;
+            int row=pq.top().second.first;
+            int col = pq.top().second.second;
+            pq.pop();
+            ans.push_back(small);
+            if(col+1<k){
+                pq.push({arr[row][col+1], {row, col+1}});
+            }
+        }
+        return ans;
+        
+    }
+};
+
+
+// ---------------------------------------------------------------------------
+
 class Solution {
   public:
     // Function to merge k sorted arrays.
