eca838e3b3f28a6e8822103bd7a2fac6b9931dc5
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-06-25 12:27:31 +0530

    nxtw potd

--- Combination Sum II.cpp
+++ Combination Sum II.cpp
@@ -1,3 +1,45 @@
+//rev:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+    void helper(int idx, int target, int n, vector<int>& arr, vector<int>& temp, set<vector<int>> &st){
+        if(idx==n){if(target==0){
+            sort(temp.begin(),temp.end());
+            st.insert(temp);return;
+        }}
+        if(target==0){
+            sort(temp.begin(),temp.end());
+            st.insert(temp);return;
+        }
+            
+        
+        for(int i=idx;i<n;i++){
+            if(i!=idx && arr[i]==arr[i-1]){continue;}
+            temp.push_back(arr[i]);
+            helper(i+1,target-arr[i],n,arr,temp,st);
+            temp.pop_back();
+            // helper(i+1,target,n,arr,temp,st);
+        }
+    }
+    vector<vector<int>> findUniqueCombinations(int n, vector<int>& arr, int target) {
+        vector<vector<int>> ans; vector<int> temp;
+        set<vector<int>> st;
+        sort(arr.begin(),arr.end());
+        helper(0, target,n, arr,  temp,st);
+        for(auto it: st){
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
+
+// -------------------------------------------------------------------------------------------------
+
+
+
 class Solution {
 public:
     void helper(int ind, int sum, vector<int>& candidates, int target, vector<int>& list, vector<vector<int>>& ans) {
