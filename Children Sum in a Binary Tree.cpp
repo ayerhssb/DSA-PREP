5ccaa6883780fe374fe4d712655265bdc3c6195a
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-06-29 12:07:33 +0530

    nxtw potd

--- Children Sum in a Binary Tree.cpp
+++ Children Sum in a Binary Tree.cpp
@@ -1,3 +1,47 @@
+//rev:
+
+
+#include <bits/stdc++.h>
+using namespace std;
+/*
+class Node {
+public:
+  int data;
+  Node *left, *right;
+
+  Node(int data) {
+      this->data = data;
+      left = right = nullptr;
+  }
+};
+*/
+class solution{
+public:
+    bool helper(Node* root){
+        if(!root){return true;}
+        int sum=0;
+        if(root->left || root->right){
+            if(root->left){sum+=root->left->data;}
+            if(root->right){sum+=root->right->data;}
+        }
+        else{sum=root->data;}
+        if(sum != root->data){return false;}
+        
+        if(!helper(root->left)){return false;}
+        if(!helper(root->right)){return false;}
+        
+        return true;
+    }
+    bool childrenSumProperty(Node* root) {
+        if(!root){return true;}
+        return helper(root);
+    }
+};
+
+
+// -----------------------------------------------------------------------------------
+
+
 /*
 
 class Node {
