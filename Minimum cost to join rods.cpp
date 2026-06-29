fb6d616a1efe8ff5cb075259f831203c72452c6c
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-06-29 12:18:56 +0530

    nxtw potd

--- Minimum cost to join rods.cpp
+++ Minimum cost to join rods.cpp
@@ -4,6 +4,33 @@ using namespace std;
 class solution {
 public:
    int minCostToJoinRods(vector<int>& arr) {
+        // min heaps
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int sum=0;
+        for(int i=0;i<arr.size();i++){
+            pq.push(arr[i]);
+        }
+        while(pq.size()>1){
+                int top1 = pq.top(); pq.pop();
+                int top2 = pq.top();pq.pop();
+                int sum1=(top1+top2);
+                sum+= sum1;
+                pq.push(sum1);
+        }
+        
+        return sum;
+    }
+};
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+   int minCostToJoinRods(vector<int>& arr) {
         int tsum=0, n=arr.size();
         priority_queue<int, vector<int>, greater<>> pq;
         for(int i=0;i<n;i++){
