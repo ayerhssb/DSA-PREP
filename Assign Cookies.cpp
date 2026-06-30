0c20ddab5ba0fd773d837946aa357e2504569b8c
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-06-30 23:11:43 +0530

    nxtw potd

--- Assign Cookies.cpp
+++ Assign Cookies.cpp
@@ -1,3 +1,29 @@
+//rev:
+
+#include <bits/stdc++.h>
+using namespace std;
+class solution {
+public:
+    int assignCookies(vector<int>& greed, vector<int>& sizes) {
+        int n=greed.size(), m=sizes.size();
+        sort(greed.begin(), greed.end());
+        sort(sizes.begin(),sizes.end());
+       int cnt=0, i=0,j=0;
+        while(i<n&&j<m){
+            if(greed[i]<=sizes[j]){
+                cnt++;i++;j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return cnt;
+    }
+};
+
+
+// -------------------------------------------------------------------------------
+
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
