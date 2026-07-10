803820a72cf8ffefb7a3d32d10859c79803ac126
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-07-10 22:23:20 +0530

    nxtw potd

--- Single Element.cpp
+++ Single Element.cpp
@@ -1,3 +1,26 @@
+//rev --> but use xor solution
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+    int singleNumber(vector<int> &arr) {
+        unordered_map<int,int> mpp;
+        for(int i=0;i<arr.size();i++){
+            mpp[arr[i]]++;
+        }
+        for(auto it:mpp){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+
+};
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
