017a0f6919fbe9d09a72358d75372639397c7613
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-07-13 21:17:11 +0530

    nxtw potd
    easy

--- N meetings in one room.cpp
+++ N meetings in one room.cpp
@@ -1,3 +1,38 @@
+//revision
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+public:
+    int conferenceRoomBooking(vector<int>& begin, vector<int>& finish) {
+        int ans=1;
+        vector<pair<int, int>> time;
+        for(int i=0;i<begin.size();i++){
+            time.push_back({begin[i], finish[i]});
+        }
+        sort(time.begin(), time.end(), [&](const pair<int,int> a, const pair<int,int> b){
+            if(a.second==b.second){
+                return a.first<b.first;
+            }
+            return a.second<b.second;
+        });
+        int i=1;
+        int lastend=time[0].second;
+        while(i<begin.size()){
+            if(time[i].first > lastend){
+                ans++;
+                lastend=time[i].second;
+            }
+            i++;
+        }
+        return ans;
+    }
+};
+
+
+//---------------------------------------------------------------------------------------
+
 class Solution {
   public:
     // Function to find the maximum number of meetings that can
