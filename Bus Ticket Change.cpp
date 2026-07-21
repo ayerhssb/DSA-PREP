524aa9909e9cb6728450773cef5f1a7c2c475706
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-07-21 11:01:54 +0530

    nxtw potd

--- Bus Ticket Change.cpp
+++ Bus Ticket Change.cpp
@@ -1,3 +1,40 @@
+//again:
+
+#include<bits/stdc++.h>
+using namespace std;
+class solution {
+public:
+    bool movieTicketCounter(vector<int>& tickets) {
+        int fifty=0, hundred=0, twohundred=0, n=tickets.size();
+        for(int i=0;i<n;i++){
+            if(tickets[i]==100){
+                if(fifty==0){
+                return false;}
+                else{
+                    hundred++;
+                    fifty--;
+                }
+            }
+            else if(tickets[i]==200){
+                if(hundred>=1 && fifty>=1){
+                    twohundred++; fifty--; hundred--;
+                }
+                else if(fifty>=3){
+                    twohundred++; fifty-=3;
+                }
+                else {
+                    return false;
+                }
+            }
+            else{
+                fifty++;
+            }
+        }
+        return true;
+    }
+};
+// -------------------------------------------------------------------------------------------------
+
 class Solution {
   public:
     bool canServe(vector<int> &arr) {
