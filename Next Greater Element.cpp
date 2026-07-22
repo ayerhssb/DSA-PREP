899b7c570211fc0058bdb54eb3ddf0b93aae149d
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-07-22 12:26:05 +0530

    nxtw potd

--- Next Greater Element.cpp
+++ Next Greater Element.cpp
@@ -1,3 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+    public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums2.size();
+        vector<int> ans;
+        unordered_map<int,int> mpp;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums2[j]>nums2[i]){
+                    mpp[nums2[i]]=nums2[j];
+                    // flag=1;
+                    break;
+                }
+            }
+            // if(flag==0){mpp[nums2[i]]=-1;}
+        }
+        
+        for(int i=0;i<nums1.size();i++){
+            if(mpp.find(nums1[i])==mpp.end()){
+                ans.push_back(-1);continue;
+            }
+            ans.push_back(mpp[nums1[i]]);
+        }
+        return ans;
+    }
+};
+
+// ---------------------------------------------------------------------------------------------------
+
 #include<bits/stdc++.h>
 using namespace std;
 
