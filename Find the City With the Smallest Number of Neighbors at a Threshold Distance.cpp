82d0b6a7e6368aff80d5dbb484f148147d126560
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-07-23 23:21:39 +0530

    nxtw potd

--- Find the City With the Smallest Number of Neighbors at a Threshold Distance.cpp
+++ Find the City With the Smallest Number of Neighbors at a Threshold Distance.cpp
@@ -1,3 +1,54 @@
+//rev:
+//Floyd Warshall Algo
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+   public:
+    int citySmallestNeighbours(int n, vector<vector<int>>& edges, int maxDistance) {
+        // vector<vector<pair<int, int>>> adj(n);
+        vector<vector<int>> grid(n, vector<int>(n, INT_MAX));
+        for (int i = 0; i < edges.size(); i++) {
+            // adj[edges[i][0]].push_back({edges[i][1], edges[i][2]});
+            grid[edges[i][0]][edges[i][1]] = edges[i][2];
+            grid[edges[i][1]][edges[i][0]] = edges[i][2];
+        }
+
+        for (int i = 0; i < n; i++) {
+            grid[i][i] = 0;
+        }
+
+        for (int via = 0; via < n; via++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][via]!=INT_MAX && grid[via][j]!=INT_MAX && grid[i][j] > grid[i][via] + grid[via][j]) {
+                        grid[i][j] = grid[i][via] + grid[via][j];
+                    }
+                }
+            }
+        }
+        int ans=0, mincnt=INT_MAX;
+        for (int i = 0; i < n; i++) {
+            int cnt=0;
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j]<=maxDistance) {
+                    cnt++;
+                }
+            }
+            if(cnt<=mincnt){
+                mincnt=cnt; ans=i;
+            }
+        }
+        return ans;
+    }
+};
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
 class Solution {
 public:
     void floydwarshall(vector<vector<int>>& mat, int n) {
