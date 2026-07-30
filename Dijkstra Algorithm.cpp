788a3556ff9bb6b5afe33f1ad32e32f0c02ac139
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-07-30 21:30:42 +0530

    nxtw potd

--- Dijkstra Algorithm.cpp
+++ Dijkstra Algorithm.cpp
@@ -1,3 +1,33 @@
+//rev:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+public:
+    vector<int> dijkstra(int n, vector<vector<pair<int, int>>>& adj, int start) {
+        vector<int> dist(n,1e9);
+        dist[start]=0;
+        priority_queue<pair< int, int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        pq.push({0, start});
+        while(!pq.empty()){
+            int node= pq.top().second;
+            int d=pq.top().first;
+            pq.pop();
+            for(auto it: adj[node]){
+                if(dist[node]+it.second < dist[it.first]){
+                    dist[it.first] = dist[node] + it.second;
+                    pq.push({dist[it.first], it.first});
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+
+// -----------------------------------------------------------------------------------------------------------
+
 // User Function Template
 class Solution {
   public:
