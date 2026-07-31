895d592479b1e984afddd6b24b72e889066dcfbe
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-07-31 21:21:01 +0530

    nxtw potd

--- Disjoint set (Union-Find).cpp
+++ Disjoint set (Union-Find).cpp
@@ -1,3 +1,39 @@
+//Union By Size::
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+    int find(int node, vector<int>& parent) {
+        if(parent[node]<0){
+            return node;
+        }
+        return parent[node] = find(parent[node], parent);
+    }
+
+    void unionSize(int a, int b, vector<int>& parent) {
+        int parentofa= find(a, parent);
+        int parentofb = find(b, parent);
+        
+        if(parentofa==parentofb){
+            return ;
+        }
+        if(parent[parentofa]< parent[parentofb]){
+            parent[parentofa] += parent[parentofb];
+            parent[parentofb] = parentofa;
+        }
+        else{
+            parent[parentofb] += parent[parentofa];
+            parent[parentofa] = parentofb;
+        }
+        
+    }
+};
+
+
+
 /*Complete the functions below*/
 int find(int par[], int x) {
     // add code here
