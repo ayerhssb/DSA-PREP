b9fc881f2495ba50919145d535ad0748d84c2d2d
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-08-01 23:10:25 +0530

    nxtw potd

--- Selection Sort Implementation.cpp
+++ Selection Sort Implementation.cpp
@@ -1,3 +1,27 @@
+//descending order using selection sort --> O(n^2) TC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+    public:
+    void selectionSort(int arr[], int n){
+        for(int i=0;i<n;i++){
+            int maxi=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]>arr[maxi]){
+                    maxi=j;
+                }
+            }
+            swap(arr[i], arr[maxi]);
+        }
+    }
+};
+
+// --------------------------------------------------------------------------------
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
