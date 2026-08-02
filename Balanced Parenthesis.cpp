2598e08ced732a80c5c0c2b52457bfe2f15ba67d
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-08-02 16:43:13 +0530

    nxtw potd

--- Balanced Parenthesis.cpp
+++ Balanced Parenthesis.cpp
@@ -1,3 +1,30 @@
+//used if else:  --> always use stack::
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class solution {
+public:
+    bool balancedParentheses(string s) {
+        int curlopen=0, curlclosed=0, roundopen=0, roundclosed=0, squareopen=0, squareclosed=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){roundopen++;}
+            else if(s[i]==')'){roundopen--;}
+            else if(s[i]=='{'){curlopen++;}
+            else if(s[i]=='}'){curlopen--;}
+            else if(s[i]=='['){squareopen++;}
+            else{squareopen--;}
+            
+            if(roundopen<0 || squareopen < 0 || curlopen<0){return false;}
+        }
+        if(roundopen>0 || squareopen > 0 || curlopen>0){return false;}
+        return true;
+    }
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
 #include<bits/stdc++.h>
 using namespace std;
 class solution {
