30201c408e002d0de42ce5b372403dd56e5351f4
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-08-05 10:58:38 +0530

    nxtw potd

--- Flood Fill.cpp
+++ Flood Fill.cpp
@@ -1,3 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+    public:
+    int dir_x[4]={1,-1, 0,0};
+    int dir_y[4]= {0,0,1,-1};
+    void helper(int row, int col, int color, int newColor, vector<vector<int>>& image, int m, int n){
+        // vis[row][col]=1;
+        image[row][col] = newColor;
+        for(int i=0;i<4;i++){
+            int nr= row+ dir_x[i];
+            int nc = col + dir_y[i];
+            if(nc>=0&&nc<n && nr>=0 && nr<m && image[nr][nc]==color){
+                image[nr][nc] = newColor;
+                helper(nr,nc, color, newColor, image,m,n);
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int r, int c,int newColor){
+        int m=image.size(), n=image[0].size();
+        helper(r, c, image[r][c], newColor, image, m,n);
+        return image;
+    }
+};
+
+// ---------------------------------------------------------------------------------------------------------------
+
+
 class Solution {
 public:
     void dfs(int inCol, int color, int x, int y, int n, int m,
