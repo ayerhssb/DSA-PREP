aa4a36846a1b9c4b57501dc861674e19bea2070d
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-08-11 21:12:42 +0530

    nxtw potd

--- Linked List Cycle.cpp
+++ Linked List Cycle.cpp
@@ -1,3 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+class Node {
+    public:
+        int data;
+        Node* next;
+    public:
+        Node(int value, Node* address){
+            data = value;
+            next = address;
+        }
+    public:
+        Node(int value){
+            data = value;
+            next = NULL;
+        }
+};
+*/
+
+class solution {
+public:
+    bool cycleDetection(Node* head){
+        Node* slow=head;
+        Node* fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+
+};
+
+
+// ---------------------------------------------------
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
