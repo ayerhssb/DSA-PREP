f6cb861a15fb6eb060b42478a024f98cdcd643e8
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-08-20 20:11:22 +0530

    sliding window

--- Fruits & Baskets.cpp
+++ Fruits & Baskets.cpp
@@ -1,3 +1,29 @@
+//rev::
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int l=0,r=0, num=2,ans=INT_MIN;
+        unordered_map<int,int> mpp;
+        while(r<fruits.size()){
+            mpp[fruits[r]]++;
+            if(mpp.size()>2){
+                int n=fruits[l];
+                while(fruits[l]==n){mpp[fruits[l]]--;
+                if(mpp[fruits[l]]==0){mpp.erase(fruits[l]);}  
+                l++;}
+            }
+            ans=max(ans, r-l+1);
+            // if(mpp.find(fruits[r])==mpp.end()){mpp[fruits[r]]++;}
+            r++;
+        }
+        return ans;
+    }
+};
+
+// ------------------------------------------------------------------------------------------
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
