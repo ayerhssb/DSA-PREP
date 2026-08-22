b36a640a8743d63d53b1838bbeb2d861dd62a322
Shreya Bharti <123381332+ayerhssb@users.noreply.github.com>
2026-08-22 16:08:25 +0530

    sliding window

--- Longest Repeating Character Replacement.cpp
+++ Longest Repeating Character Replacement.cpp
@@ -1,3 +1,26 @@
+//rev::
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l=0, r=0, ans=0; int mostfreq=0;
+        unordered_map<int, int> mpp;
+        while(r<s.size()){
+            mpp[s[r]]++;
+            if(mpp[s[r]]>mostfreq){mostfreq=mpp[s[r]];}
+            int len = r-l+1;
+            if(len-mostfreq <=k){
+                ans = max(ans, len);
+            }
+            else{if(mpp[s[l]]==mostfreq){mostfreq--;} mpp[s[l]]--;l++;}
+            r++;
+        }
+        return ans;
+    }
+};
+
+
+
 class Solution {
 public:
     int characterReplacement(string s, int k) {
